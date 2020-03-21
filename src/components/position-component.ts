57dd6c843ef0eb1f4bdfa2462488d8a87ec3e9a8
vdonadze <vakho159@gmail.com>
2020-03-22 00:49:45 +0100

    removed jason ignore

--- src/components/position-component.ts
+++ src/components/position-component.ts
@@ -1,12 +1,11 @@
 import { ComponentType } from "../enums/component-type";
-import { jsonIgnoreReplacer, jsonIgnore } from 'json-ignore';
 import { SocketData } from "../helpers/decorators";
 import { BaseComponent } from "./base-component";
 
 export class PositionComponent extends BaseComponent {
-    constructor(){
+    constructor() {
         super()
-        this.componentType = ComponentType.Position 
+        this.componentType = ComponentType.Position
     }
 
     @SocketData
