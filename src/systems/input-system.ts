da19f3932de488a487656038ddc36b96b02fbc2c
Bartha Bálint <bartha.balint@hotmail.com>
2020-03-25 19:23:47 +0100

    empty delta

--- src/systems/input-system.ts
+++ src/systems/input-system.ts
@@ -7,26 +7,28 @@ export class InputSystem implements ISystem{
     private players: IPlayer[];
     private entityPool: EntityPool;
     
-    directionChanges:Map<string, number> = new Map()
+    deltaDirections:Map<string, number> = new Map()
     
     constructor(players: IPlayer[], entityPool:EntityPool){
         this.players = players
         this.entityPool = entityPool
         this.players.map(player => {
-            this.directionChanges.set(player.id, 0)
+            this.deltaDirections.set(player.id, 0)
             player.socket.on(SocketEvents.SLIDER_CHANGE, ({ value }) => this.onValueChange(player.id, value))
             player.socket.on(SocketEvents.DISCONNECT, () => console.log("Leave room"))
         })
     }
     calculateNextState(): void {
         this.entityPool.movementManager.forEach(m => {
-            m.setDeltaDirection(this.directionChanges.get(this.players[0].id))
+
+            m.setDeltaDirection(this.deltaDirections.get(this.players[0].id))
+            this.deltaDirections.set(this.players[0].id, 0)
         });
     }
 
     onValueChange(playerId:string, value:any){
         console.log(value)
-        this.directionChanges.set(playerId, value)
+        this.deltaDirections.set(playerId, value)
     }
 
 }
