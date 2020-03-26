6c9d2c06249faa0f6c540b7f3d47194107fe198b
Bartha Bálint <bartha.balint@hotmail.com>
2020-03-26 18:17:10 +0100

    movement calculation fix

--- src/components/movement-component.ts
+++ src/components/movement-component.ts
@@ -1,26 +1,26 @@
-import {IComponent} from "../interfaces/component-interfaces";
-import {ComponentType} from "../enums/component-type";
-import {BaseComponent} from "./base-component";
-import {SocketData} from "../helpers/decorators";
-import {Vector2} from "../models/position";
+import { IComponent } from "../interfaces/component-interfaces";
+import { ComponentType } from "../enums/component-type";
+import { BaseComponent } from "./base-component";
+import { SocketData } from "../helpers/decorators";
+import { Vector2 } from "../models/position";
 import { GameConstants, SnakeConstants } from "../constants";
 
 export class MovementComponent extends BaseComponent {
     constructor() {
         super()
         this.componentType = ComponentType.Movement
-        this.direction = new Vector2(0,0)
+        this.direction = new Vector2(0, 0)
     }
 
     directionValue = 0
 
-    direction:Vector2
+    direction: Vector2
 
-    setDeltaDirection(delta:number){
-        this.directionValue += delta
+    setDeltaDirection(delta: number) {
+        this.directionValue = (this.directionValue + delta) % 8
         this.direction = SnakeConstants.directions[this.directionValue]
     }
 
     // ( tick / block )
-    speed:number
+    speed: number
 }
