1d5bf52e905643535e56baeae43fe143af9e4ebc
Tom Pégeot <tom-pegeot@hotmail.fr>
2020-04-23 10:02:38 +0200

    Change the input and movement system to swipe & add event

--- src/components/movement-component.ts
+++ src/components/movement-component.ts
@@ -14,14 +14,18 @@ export class MovementComponent extends BaseComponent {
 
     direction: Vector2
 
-    setDeltaDirection(delta: number) {
-        this.directionValue = Number(this.directionValue) + Number(delta)
-        this.directionValue = this.directionValue < 0 ?
-            7 - ((-1 * this.directionValue) % 8)
-            : (this.directionValue) % 8
-        this.direction = SnakeConstants.directions[this.directionValue]
-    }
 
+    setDirection(dir: string){
+        if(dir == "up"){
+          this.direction = SnakeConstants.directions[0]
+        }else if(dir == "left"){
+          this.direction = SnakeConstants.directions[6]
+        }else if(dir == "down"){
+          this.direction = SnakeConstants.directions[4]
+        }else if(dir == "right"){
+          this.direction = SnakeConstants.directions[2]
+        }
+    }
     // ( tick / block )
     speed: number
 }
